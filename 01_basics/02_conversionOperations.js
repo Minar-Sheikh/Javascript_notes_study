d145de96eb3d3ddae7570f2be9c2403ecd4d881d
Minar-Sheikh <sheikhminar849@gmail.com>
2025-05-13 18:48:10 +0000

    conversions of dataTypes

---
+++ 01_basics/02_conversionOperations.js
@@ -0,0 +1,28 @@
+let number= "33abc"
+
+console.log(typeof number)
+
+let numberValue= Number(number)
+
+console.log(typeof numberValue)
+console.log(numberValue)
+
+// "33" => 33
+// "33abc"=>  NaN
+// true => 1 , false => 0
+
+let isLoggedIn= "Minar"
+
+let boleanValue= Boolean(isLoggedIn)
+console.log(boleanValue);
+
+// 0 => false , 1 => true
+// "" => false
+// "Minar" => true
+
+
+let someNumber = 33
+
+let stringValue = String(someNumber)
+console.log(someNumber);
+console.log(typeof someNumber);
