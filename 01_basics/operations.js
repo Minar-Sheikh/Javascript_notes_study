4ca775d3437ff3f53f7f64f878cebb3cf9dc3d8e
Minar-Sheikh <sheikhminar849@gmail.com>
2025-05-13 19:13:38 +0000

    Math Operations simple

---
+++ 01_basics/operations.js
@@ -0,0 +1,33 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = "Minar";
+let str2 = " Sheikh";
+
+let  str3 = str1+str2
+console.log(str3);
+
+console.log(1 + "2"); //12
+console.log("1" + 2);    //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
+
+console.log( (3+2)*5%3);
+
+console.log(+true) //  1
+console.log(-true); //   -1
+console.log(+false); //  0
+console.log(-false); //  -0
+
+console.log(-""); //  -0
+
+let gameCount = 100;
+gameCount++;
+console.log(gameCount); // 101
+
+let postIncrement = 100;
+++postIncrement;
+console.log(postIncrement);// 101
+
