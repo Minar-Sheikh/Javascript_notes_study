0f7b279b65fc1a839849e849dedad3a5fdf4d8dc
Minar-Sheikh <sheikhminar849@gmail.com>
2025-05-13 19:34:38 +0000

    comparison operators

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+console.log("1" > 2); // false 
+// JS convert 1 to number for comparision
+
+console.log(null > 0); // false
+console.log(null == 0); // false
+// Equality operator can't convert null to 0
+
+console.log(null >= 0); // true
+// Comparision operator convert null to 0 and the check 0 = 0 that is true
+//In JS comparision check(>,<) works diffrently then Equality operator (==)
+
+console.log(undefined > 0 ) // False
+console.log(undefined = 0 )//false
+console.log(undefined < 0 )//false
+
+// === this operator check value as well as there data type
+console.log("2" === 2) //false
+console.log( 2 === 2 ) // true 
+
+
+
+
+
